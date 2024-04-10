f3eb4657891464f915c1b70daff2df2ff8807b03
Aneesha Rama <aneesharama@gmail.com>
2024-04-11 01:45:21 +0530

    backend: added video upload feature

---
+++ backend/user_profile_service/Models/Video.cs
@@ -0,0 +1,9 @@
+namespace user_profile_service.Models
+{
+    public class Video
+    {
+        public string videoId { get; set; }
+        public string userId { get; set; }
+
+    }
+}
--- backend/user_profile_service/Services/IUserProfileService.cs
+++ backend/user_profile_service/Services/IUserProfileService.cs
@@ -5,7 +5,7 @@ namespace user_profile_service.Services
 {
     public interface IUserProfileService
     {
-        Task<bool> AddToMyVideos(string videoId, string userId);
+        Task<bool> AddToMyVideos(Video video);
         Task<bool> CreteUserProfile(User user);
     }
 }
