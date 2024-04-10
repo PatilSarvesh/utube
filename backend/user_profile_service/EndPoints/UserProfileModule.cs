5d47b2697da1aaee63b561f305912f493cfc298a
Sarvesh Patil <patilsarvesh1999@gmail.com>
2024-04-11 00:55:33 +0530

    backend: added Saving video feature

--- backend/user_profile_service/EndPoints/UserProfileModule.cs
+++ backend/user_profile_service/EndPoints/UserProfileModule.cs
@@ -6,14 +6,21 @@ namespace user_profile_service.EndPoints
 {
     public class UserProfileModule : ICarterModule
     {
-        public void AddRoutes(IEndpointRouteBuilder app)
+        public UserProfileModule() : base("/api/user-profile-service") { }
+
+        public override void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapPost("/api/user-profile-service/create-profile", async (IUserProfileService userProfileService, User user) =>
+            app.MapPost("/create-profile", async (IUserProfileService userProfileService, User user) =>
             {
                 var userProfile = await userProfileService.CreteUserProfile(user);
                 return Results.Json(userProfile);
             });
 
+            app.MapPost("/add-to-my-video", async (IUserProfileService userProfileService, string videoId, string userId) =>
+            {
+                 await userProfileService.AddToMyVideos(videoId, userId);
+                return Results.Ok();
+            });
         }
     }
 }
