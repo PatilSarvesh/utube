4aa37ff51f24fdf567834b1616a1cee8a7552a88
Sarvesh Patil <patilsarvesh1999@gmail.com>
2024-04-17 21:45:13 +0530

    backend: Update UserProfile.cs

--- backend/user_profile_service/Models/UserProfile.cs
+++ backend/user_profile_service/Models/UserProfile.cs
@@ -5,7 +5,7 @@
         public string Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
         public DateTime CreatedOn { get; set; }
 
         public List<string> MyVideos { get; set; }
